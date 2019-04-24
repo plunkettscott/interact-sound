2e15d2f1e4fcfd2cd92c23d9450fc37ce454314b
Scott Plunkett <scottplunkett15@gmail.com>
2019-04-24 12:07:22 -0500

    fix: syntax in __resource.lua was incorrect

--- __resource.lua
+++ __resource.lua
@@ -12,9 +12,9 @@ ui_page('client/html/index.html')
 
 -- Files needed for NUI
 -- DON'T FORGET TO ADD THE SOUND FILES TO THIS!
-files({
+files {
     'client/html/index.html',
     -- Begin Sound Files Here...
     -- client/html/sounds/ ... .ogg
     'client/html/sounds/demo.ogg'
-})
+}
