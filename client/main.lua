20dfafedba0603433aea91450b721934f11bf799
Lewis <13007790+The-Neco@users.noreply.github.com>
2020-07-30 04:52:00 +0100

    Updated to work with onesync infinity

--- client/main.lua
+++ client/main.lua
@@ -79,3 +79,33 @@ AddEventHandler('InteractSound_CL:PlayWithinDistance', function(playerNetId, max
         })
     end
 end)
+
+
+------
+-- RegisterNetEvent LIFE_CL:Sound:PlayWithinDistanceOS (Bigmode varient of InteractSound_CL:PlayWithinDistance)
+--
+-- @param playerCoords    - The coords of the player making the sound
+--
+-- @param maxDistance     - The maximum float distance (client uses Vdist) to allow the player to
+--                        - hear the soundFile being played.
+-- @param soundFile       - The name of the soundfile within the client/html/sounds/ folder.
+--                        - Can also specify a folder/sound file.
+-- @param soundVolume     - The volume at which the soundFile should be played. Nil or don't
+--                        - provide it for the default of standardVolumeOutput. Should be between
+--                        - 0.1 to 1.0.
+--
+-- Starts playing a sound on a client if the client is within the specificed maxDistance from the playOnEntity.
+-- @TODO Change sound volume based on the distance the player is away from the playOnEntity.
+------
+RegisterNetEvent('InteractSound_CL:PlayWithinDistanceOS')
+AddEventHandler('InteractSound_CL:PlayWithinDistanceOS', function(playerCoords, maxDistance, soundFile, soundVolume)
+    local lCoords = GetEntityCoords(GetPlayerPed(-1))
+    local distIs  = Vdist(lCoords.x, lCoords.y, lCoords.z, playerCoords.x, playerCoords.y, playerCoords.z)
+    if(distIs <= maxDistance) then
+        SendNUIMessage({
+            transactionType     = 'playSound',
+            transactionFile     = soundFile,
+            transactionVolume   = soundVolume
+        })
+    end
+end)
