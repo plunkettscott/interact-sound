16921dede66c0ef3cefbbaa31677dbd51964ab9d
Lewis <13007790+The-Neco@users.noreply.github.com>
2020-07-30 04:50:35 +0100

    Updated to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,6 @@
--- Manifest Version
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+-- FXVersion Version
+fx_version 'adamant'
+games { 'gta5' }
 
 -- Client Scripts
 client_script 'client/main.lua'
@@ -8,7 +9,7 @@ client_script 'client/main.lua'
 server_script 'server/main.lua'
 
 -- NUI Default Page
-ui_page('client/html/index.html')
+ui_page "client/html/index.html"
 
 -- Files needed for NUI
 -- DON'T FORGET TO ADD THE SOUND FILES TO THIS!
