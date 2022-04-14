12076645cc8795b7ff49c6c9f29d4174c898a6a9
Mustafa Burak Güneş <54374780+TGIANN@users.noreply.github.com>
2022-04-14 17:54:08 +0300

    Make it easier to add sound

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -13,10 +13,7 @@ server_script 'server/main.lua'
 ui_page "client/html/index.html"
 
 -- Files needed for NUI
--- DON'T FORGET TO ADD THE SOUND FILES TO THIS!
 files {
     'client/html/index.html',
-    -- Begin Sound Files Here...
-    -- client/html/sounds/ ... .ogg
-    'client/html/sounds/demo.ogg'
+    'client/html/sounds/*.ogg'
 }
